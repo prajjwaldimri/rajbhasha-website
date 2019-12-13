064fe181eac863fdfecef0643df8d1a6aa0d01fe
Prajjwal Dimri <prajjwaldimri@hotmail.com>
2019-12-13 18:41:10 +0530

    Added floating nav

---
+++ src/components/FloatingNav.vue
@@ -0,0 +1,6 @@
+<template>
+  <v-toolbar floating extended>
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-toolbar-title>Title</v-toolbar-title>
+  </v-toolbar>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <HelloWorld />
+    <FloatingNav />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import FloatingNav from "@/components/FloatingNav.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    HelloWorld,
+    FloatingNav
   }
 };
 </script>
