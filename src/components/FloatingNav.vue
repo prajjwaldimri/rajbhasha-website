806cfd82ef251c556973ca4c0a5e4fc0ea0bc1fe
Prajjwal Dimri <prajjwaldimri@hotmail.com>
2019-12-26 14:34:26 +0530

    Added transition to bottom nav

--- src/components/FloatingNav.vue
+++ src/components/FloatingNav.vue
@@ -1,11 +1,13 @@
 <template lang="pug">
   v-toolbar(absolute bottom).floatingNav
     v-toolbar-items.floatingNavItems
-      v-btn(text) Hamare Baare main
+      v-btn(text).nav-1 Hamare Baare main
       v-btn(text) Karyakalap
       v-btn(text) Sansaadhan
       v-btn(text) Avashyak Links
       v-btn(text) Sampark Karein
+
+    .expanded-menu
 </template>
 
 <style lang="scss">
@@ -19,4 +21,34 @@
   width: 100%;
   justify-content: space-around;
 }
+
+.expanded-menu {
+  &.active {
+    opacity: 100%;
+  }
+  opacity: 0;
+  height: 200%;
+  width: 100%;
+  position: absolute;
+  background: #424242;
+  transform: translateY(-80%) translateX(-1rem);
+  transition: all 300ms;
+}
 </style>
+
+<script>
+export default {
+  mounted() {
+    const menu = document.getElementsByClassName("expanded-menu")[0];
+    const floatingNavItem = document.getElementsByClassName("nav-1")[0];
+
+    floatingNavItem.addEventListener("mouseover", () => {
+      menu.classList.add("active");
+    });
+
+    menu.addEventListener("mouseout", () => {
+      menu.classList.remove("active");
+    });
+  }
+};
+</script>
