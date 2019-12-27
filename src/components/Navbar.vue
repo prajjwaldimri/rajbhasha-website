042a00c96eaf5104149530b39ae21ca8f5f2cf2f
Subarna Dhar <subarnadhar22@gmail.com>
2019-12-27 16:43:28 +0530

    nit logo added

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,4 +1,21 @@
 <template lang="pug">
   v-toolbar(flat color="transparent")
-    v-toolbar-title राष्ट्रीय प्रौद्योगिकी संस्थान गोवा 
+    v-toolbar-title 
+      .logo
+        v-img(src="@/assets/nitlogo.png" height="24" width="24" )   
+        span राष्ट्रीय प्रौद्योगिकी संस्थान गोवा
 </template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.logo {
+  span {
+    padding-left: 0.5rem;
+  }
+  display: flex;
+  align-items: center;
+}
+</style> 
