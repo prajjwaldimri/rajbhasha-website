b9289f27eaa47a01f3a69d3167af40dea89189b0
kaundinya <kaundinya77@gmail.com>
2019-12-28 21:11:38 +0530

    Added Tables : HindiDayProgram, OfficeOrder, Workshop
    Issue : Added table but they were not reflecting between NavBar and FloatingNav

---
+++ src/views/OfficeOrder.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-table :data="users">
+    <thead slot="head">
+      <th>Date</th>
+      <th>Subject</th>
+      <th>Office Order/Circular</th>
+    </thead>
+    <tbody slot="body" slot-scope="{ displayData }">
+      <tr v-for="row in displayData" :key="row.id">
+        <td>{{ row.date }}</td>
+        <td>{{ row.subject }}</td>
+        <td>{{ row.office_order }}</td>
+      </tr>
+    </tbody>
+  </v-table>
+</template>
+
+<script>
+import users from "./users.json";
+
+export default {
+  name: "TheBasics",
+  data: () => ({
+    users
+  })
+};
+</script>
