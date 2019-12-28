73dbf194d09c15214d79fba5b9bc7771c0fd5dac
Subarna Dhar <subarnadhar22@gmail.com>
2019-12-28 19:02:08 +0530

    added Links file to index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import About from "../views/About.vue";
+import Links from "../views/Links.vue";
 
 Vue.use(VueRouter);
 
@@ -15,6 +16,11 @@ const routes = [
     path: "/about",
     name: "about",
     component: About
+  },
+  {
+    path: "/links",
+    name: "links",
+    component: Links
   }
 ];
 
