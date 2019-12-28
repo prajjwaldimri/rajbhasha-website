ff71bcb2604fc5209e108513156a997c034bbdfb
Subarna Dhar <subarnadhar22@gmail.com>
2019-12-28 22:53:28 +0530

    changes to links.vue

--- src/views/Links.vue
+++ src/views/Links.vue
@@ -1,24 +1,22 @@
 <template lang="pug">
-    .imp-link
-        Navbar
-
-        v-content
-            v-container
-                v-toolbar-title
-                    .links-text
-                        a(href="http://www.nitgoa.ac.in/")
-                            span.text-yellow sansthan ki rajbhasha prakoshtha ki timahi report
-                        a(href="href=https://rajbhasha.nic.in/hi/hindi-vocabulary")
-                            span.text-yellow hindi shabadkosh
-                        a(href="https://mantra-rajbhasha.rb-aai.in/")
-                            span.text-yellow hindi anuvad ki link
-                        a(href="http://e-mahashabdkosh.rb-aai.in/")
-                            span.text-yellow  e-mahashabadkosh ki link
-                        a(href="https://www.microsoft.com/en-in/bhashaindia/downloads.aspx")
-                            span.text-yellow microsoft indic input tool
-                        a(href="https://rajbhasha.nic.in/")
-                            span.text-yellow bharat sarkar grih mantralaya ki rajbhasha vibhag ki website  
-        FloatingNav
+  .imp-link
+    Navbar
+    v-content
+      v-container
+        .links-text
+          a(href="http://www.nitgoa.ac.in/")
+            span.text-yellow sansthan ki rajbhasha prakoshtha ki timahi report
+          a(href="href=https://rajbhasha.nic.in/hi/hindi-vocabulary")
+            span.text-yellow hindi shabadkosh
+          a(href="https://mantra-rajbhasha.rb-aai.in/")
+            span.text-yellow hindi anuvad ki link
+          a(href="http://e-mahashabdkosh.rb-aai.in/")
+            span.text-yellow  e-mahashabadkosh ki link
+          a(href="https://www.microsoft.com/en-in/bhashaindia/downloads.aspx")
+            span.text-yellow microsoft indic input tool
+          a(href="https://rajbhasha.nic.in/")
+            span.text-yellow bharat sarkar grih mantralaya ki rajbhasha vibhag ki website  
+    FloatingNav
 </template>
 <script>
 import Navbar from "@/components/Navbar.vue";
@@ -38,11 +36,12 @@ export default {
 .links-text {
   display: grid;
   grid-template-columns: repeat(2, 1fr);
-  grid-template-rows: repeat(2, 10rem);
+  grid-template-rows: repeat(2, 1fr);
   align-items: center;
-
+  justify-items: center;
+  grid-row-gap: 1rem;
   span {
-    line-break: auto;
+    line-break: loose;
     word-break: break-word;
     word-break: break-all;
     font-family: "Poppins", sans-serif !important;
