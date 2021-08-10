164b08620ffac369a5e2787ec12ba758db732061
Prajjwal Dimri <prajjwaldimri@hotmail.com>
2021-08-10 10:02:52 +0530

    Added hindi week program

--- src/views/HindiDayProgram.vue
+++ src/views/HindiDayProgram.vue
@@ -1,18 +1,46 @@
 <template lang="pug">
-  div
-    Navbar(:breadcrumbs="breadcrumbs")
-    v-content
-      v-container(fluid)
-        v-simple-table(:dense='dense' :fixed-header='fixedHeader' )
-          template(v-slot:default='')
-            thead
-              tr
-                th.text-left दिनांक
-                th.text-left विषय
-            tbody
-              tr(v-for='item in information' :key='item.name')
-                td {{ item.date }}
-                td {{ item.subject }}
+div
+  Navbar(:breadcrumbs="breadcrumbs")
+  v-content
+    v-container.pb-10(fluid)
+      .d-flex.flex-column.pt-5.pr-10.pl-10
+        h3.poppins-font-resolution.pt-3(style="text-align: justify") प्रिय बंधुगण,
+        h3.poppins-font-resolution.pt-3(style="text-align: justify") नमस्कार!
+        h3.poppins-font-resolution.pt-3(style="text-align: justify") संस्था की राजभाषा समिति अत्यंत हर्षपूर्वक यह सूचित करती है कि इस वर्ष हम "हिंदी सप्ताह २०२१" को ऑनलाइन माध्यम द्वारा आयोजित करने जा रहे है। और इस सप्ताह का आयोजन दिनांक १४ से २० सितंबर तक किया जायेगा।
+        h3.poppins-font-resolution.pt-3(style="text-align: justify") "हिंदी सप्ताह २०२१" का शुभारंभ दिनांक १४ सितंबर २०२१ को संध्याकाल ४ बजे Google Meet के माध्यम से किया जायेगा तथा इसका विवरण एवं आमंत्रण शीघ्र ही प्रेषित किया जायेगा। "हिंदी सप्ताह २०२१" के दौरान विभिन्न प्रतियोगितओं का भी आयोजन किया जायेगा एवं आप सब से अनुरोध है कि इन प्रतियोगिताओं में बढ़-चढ़ कर हिस्सा लें और कार्यक्रम को सफल बनाएँ। धन्यवाद।
+        h3.poppins-font-resolution.pt-8.text-yellow(
+          style="text-align: center; font-weight: bold"
+        ) "अभिरुचि की अभिव्यक्ति" को प्रस्तुत करने हेतु लिंक ("गूगल फ़ॉर्म "): https://docs.google.com/forms/d/1piZAIuuV7jN3XvL1JraZpPOLC1YdwgxhBoMpWifZpWc/edit
+        h3.poppins-font-resolution.pt-8(
+          style="text-align: justify; font-weight: bold"
+        ) कृपया ध्यान दें:-
+        h3.poppins-font-resolution.pt-5(style="text-align: justify") "हिंदी सप्ताह २०२१" के दौरान आयोजित किये जाने वाले कार्यक्रमों एवं प्रतियोगिताओं की सूची इस मेल के साथ संलग्न है। कृपया हमें अपनी "अभिरुचि की अभिव्यक्ति" १० सितंबर २०२१ को या उससे पहले भेजें।
+        h3.poppins-font-resolution.pt-3(style="text-align: justify") आयोजित किये जाने वाले कार्यक्रमों एवं प्रतियोगिताओं में भाग लेने के लिए आप सभी से प्रविष्टियां आमंत्रित है। कृपया हमें अपनी "अभिरुचि की अभिव्यक्ति" ("expression of interest" ) को नीचे दी गयी लिंक ("गूगल फ़ॉर्म ") पर १४ सितंबर २०२१ या उससे पहले भेजें।
+        h3.poppins-font-resolution.pt-3(style="text-align: justify") सभी प्रतियोगिताएं निर्धारित कार्यक्रम के अनुसार होंगी, हालांकि हम आपसे अनुरोध करते है की निबंध लेखन प्रतियोगिता हेतु आप अपनी प्रविष्टियाँ समय से पूर्व यानी १५ सितंबर २०२१ से पहले ही हमें हमारे इस ईमेल आईडी rajbhasha@nitgoa.ac.in पर pdf स्वरूप में भेज दे।
+        h3.poppins-font-resolution.pt-3(style="text-align: justify") ठीक इसी प्रकार, हिंदी काव्य पाठ प्रतियोगिता एवं हिंदी हास्य काव्य पाठ प्रतियोगिता हेतु भी आप अपनी प्रविष्टियाँ समय से पूर्व यानी १४ सितंबर २०२१ से पहले ही हमें भेज दे। उल्लेखित प्रतियोगिताओं हेतु आप को उपयुक्त प्रारूप में ५ मिनट की अवधि (अधिक से अधिक) का एक पूर्व-रिकॉर्डेड वीडियो भेजना होगा ।
+
+        h3.poppins-font-resolution.pt-6(style="text-align: justify") अन्त में, राजभाषा समिति, संस्था के निदेशक महोदय को इस कार्यक्रम के अनुमोदन एवं प्रोत्साहन के लिए ह्रदय से आभार प्रकट करती है।
+        h3.poppins-font-resolution.pt-6(style="text-align: justify") विनीत,
+        h3.poppins-font-resolution.pt-1(style="text-align: justify") राजभाषा समिति,
+        h3.poppins-font-resolution.pt-1.pb-10(style="text-align: justify") राष्ट्रीय प्रौद्योगिकी संस्थान गोवा
+
+      v-simple-table.mb-10(:dense="dense", :fixed-header="fixedHeader")
+        template(v-slot:default="")
+          thead
+            tr
+              th.text-left दिनांक
+              th.text-left विषय
+              th.text-left डाउनलोड
+          tbody
+            tr(v-for="item in information", :key="item.name")
+              td {{ item.date }}
+              td {{ item.subject }}
+              td
+                a(
+                  :href="item.href",
+                  target="_blank",
+                  style="color: white; text-decoration-line: underline"
+                ) {{ item.download }}
 </template>
 
 <script>
@@ -21,7 +49,7 @@ import Navbar from "@/components/Navbar.vue";
 export default {
   name: "home",
   components: {
-    Navbar
+    Navbar,
   },
 
   data: () => ({
@@ -31,56 +59,24 @@ export default {
     information: [
       //Add Code in here
       {
-        date: "१५/१०/२०१८",
-        subject: "निबंध प्रतियोगिता",
-        office_order: "Download"
+        date: "१४/०९/२०२१",
+        subject: "हिंदी सप्ताह का शुभारम्भ एवं हिंदी साहित्य का परिचय",
+        download: "डाउनलोड",
+        href: "https://drive.google.com/file/d/1JmjbwkSfcHhZNcSfnyCH4QHISAQFTtJ_/view?usp=sharing",
       },
-      {
-        date: "१०/१०/२०१८",
-        subject: "हिंदी टंकण एवं हिंदी हस्त लेखन प्रतियोगिता",
-        office_order: "Download"
-      },
-      {
-        date: "१८/०१/२०१७ ",
-        subject:
-          "हिंदी आदर्श वाक्य लेखन प्रतियोगिता तथा हिंदी सप्ताह समापन समारोह ",
-        office_order: "Download"
-      },
-      {
-        date: "१५/०१/२०१७ ",
-        subject: "वाद संवाद : वर्तमान तकनिकी में नवाचार एवं लाभान्वित समुदाय ",
-        office_order: "Download"
-      },
-      {
-        date: "१४/०१/२०१७ ",
-        subject: "हिंदी हस्त लेखन एवं हिंदी टंकण प्रतियोगिता ",
-        office_order: "Download"
-      },
-      {
-        date: "१३/०१/२०१७ ",
-        subject:
-          "स्वरचित हिंदी काव्य पाठ प्रतियोगिता एवं हिंदी हास्य काव्य पाठ प्रतियोगिता",
-        office_order: "Download"
-      },
-      {
-        date: "१२/०१/२०१७ ",
-        subject:
-          "हिंदी सप्ताह का शुभ आरंभ तथा हिंदी साहित्य का परिचय : हिंदी समिति की पेशकश",
-        office_order: "Download"
-      }
     ],
     breadcrumbs: [
       {
         text: "होम",
         disabled: false,
-        href: "/"
+        href: "/",
       },
       {
-        text: "हिंदी दिवस कार्यक्रम",
+        text: "हिंदी सप्ताह कार्यक्रम",
         disabled: true,
-        href: "/hindidayprogram"
-      }
-    ]
-  })
+        href: "/hindidayprogram",
+      },
+    ],
+  }),
 };
 </script>
--- src/views/Message.vue
+++ src/views/Message.vue
@@ -1,21 +1,25 @@
 <template lang="pug">
-  div
-    Navbar(:breadcrumbs="breadcrumbs")
-    v-content
-      v-container(fluid)
-        v-simple-table(:dense='dense' :fixed-header='fixedHeader' )
-          template(v-slot:default='')
-            thead
-              tr
-                th.text-left दिनांक
-                th.text-left विषय
-                th.text-left डाउनलोड
-            tbody
-              tr(v-for='item in information' :key='item.name')
-                td {{ item.date }}
-                td {{ item.subject }}
-                td 
-                  a(:href="item.href" target="_blank" style="color:white;text-decoration-line:underline;") {{ item.download }}
+div
+  Navbar(:breadcrumbs="breadcrumbs")
+  v-content
+    v-container(fluid)
+      v-simple-table(:dense="dense", :fixed-header="fixedHeader")
+        template(v-slot:default="")
+          thead
+            tr
+              th.text-left दिनांक
+              th.text-left विषय
+              th.text-left डाउनलोड
+          tbody
+            tr(v-for="item in information", :key="item.name")
+              td {{ item.date }}
+              td {{ item.subject }}
+              td 
+                a(
+                  :href="item.href",
+                  target="_blank",
+                  style="color: white; text-decoration-line: underline"
+                ) {{ item.download }}
 </template>
 
 <script>
@@ -24,7 +28,7 @@ import Navbar from "@/components/Navbar.vue";
 export default {
   name: "home",
   components: {
-    Navbar
+    Navbar,
   },
 
   data: () => ({
@@ -37,16 +41,14 @@ export default {
         date: "12 / 09 / 2018",
         subject: "हिंदी दिवस संदेश",
         download: "डाउनलोड",
-        href:
-          "https://drive.google.com/open?id=1Tq-oLVinv4JCllifm_cWo6ssMwEoCWat"
+        href: "https://drive.google.com/open?id=1Tq-oLVinv4JCllifm_cWo6ssMwEoCWat",
       },
       {
         date: "14 / 09 / 2016",
         subject:
           "हिंदी दिवस के अवसर पर मानव संसाधन विकास भारत सरकार मंत्री जी का हिंदी संदेश",
         download: "डाउनलोड",
-        href:
-          "https://drive.google.com/file/d/1IeDL8lXG_rlI6mMOEWIHCqsIsOqMd83z/view?usp=sharing"
+        href: "https://drive.google.com/file/d/1IeDL8lXG_rlI6mMOEWIHCqsIsOqMd83z/view?usp=sharing",
       },
 
       {
@@ -54,37 +56,34 @@ export default {
         subject:
           "हिंदी दिवस के अवसर पर भारत के राष्ट्रपति श्री प्रणब मुखर्जी का अभिभाषण",
         download: "डाउनलोड",
-        href:
-          "https://drive.google.com/file/d/1VofmU-bU7U-0tKkol93KhqMa6TpYnNLj/view?usp=sharing"
+        href: "https://drive.google.com/file/d/1VofmU-bU7U-0tKkol93KhqMa6TpYnNLj/view?usp=sharing",
       },
       {
         date: "14 / 09  / 2015",
         subject: "हिंदी दिवस के अवसर पर माननीय गृह मंत्री जी का हिंदी संदेश ",
         download: "डाउनलोड",
-        href:
-          "https://drive.google.com/open?id=1sXy13WASh82rtHkGlTL9nwZVc6WxHgUS"
+        href: "https://drive.google.com/open?id=1sXy13WASh82rtHkGlTL9nwZVc6WxHgUS",
       },
       {
         date: "14 / 09  / 2015",
         subject:
           "हिंदी दिवस के अवसर पर मानव संसाधन विकास भारत सरकार मंत्री जी का हिंदी संदेश",
         download: "डाउनलोड",
-        href:
-          "https://drive.google.com/open?id=1DMuJpZ94u_nQJba9uUvS2YEmypr_g0Xp"
-      }
+        href: "https://drive.google.com/open?id=1DMuJpZ94u_nQJba9uUvS2YEmypr_g0Xp",
+      },
     ],
     breadcrumbs: [
       {
         text: "होम",
         disabled: false,
-        href: "/"
+        href: "/",
       },
       {
         text: "संदेश",
         disabled: true,
-        href: "/messages"
-      }
-    ]
-  })
+        href: "/messages",
+      },
+    ],
+  }),
 };
 </script>
