6d4033ddd8b0bdb11807a30e0b80e00b586e25fe
Prajjwal Dimri <prajjwaldimri@hotmail.com>
2021-08-10 10:32:00 +0530

    Fixed layout of yearly magazine

--- src/views/YearlyMagazineEntry.vue
+++ src/views/YearlyMagazineEntry.vue
@@ -4,12 +4,14 @@ div
 
   v-content
     v-container
-      .d-flex.flex-column.pt-5.text-yellow
-        span.display-3(style="text-align: center; font-weight: 600") वार्षिक पत्रिका "उर्बा"
-        h2.poppins-font-resolution.pt-3(style="text-align: justify") “हिंदी में हमारे संस्थान NIT Goa की पत्रिका "उर्बा " के पहले संस्करण के लिए प्रविष्टियाँ आमंत्रित हैं / Submissions are invited for first edition of our Institute NIT Goa's magazine "Urba" in Hindi
-        h2.poppins-font-resolution.pt-3(style="text-align: justify") प्रिय बंधुगण,
+      .d-flex.flex-column.pt-10
+        span.display-3.text-yellow(
+          style="text-align: center; font-weight: 600"
+        ) वार्षिक पत्रिका "उर्बा"
+        h2.poppins-font-resolution.pt-8.text-yellow(style="text-align: center") “हिंदी में हमारे संस्थान NIT Goa की पत्रिका "उर्बा " के पहले संस्करण के लिए प्रविष्टियाँ आमंत्रित हैं / Submissions are invited for first edition of our Institute NIT Goa's magazine "Urba" in Hindi
+        h2.poppins-font-resolution.pt-10(style="text-align: justify") प्रिय बंधुगण,
         h2.poppins-font-resolution.pt-3(style="text-align: justify") राजभाषा समिति की ओर से अभिनंदन!
-        h2.poppins-font-resolution.pt-3(style="text-align: justify") हिंदी के उत्सव को हिंदी सप्ताह की निश्चित समय सीमा के अतिरिक्त और विस्तार देते हुए हिंदी पत्रिका "उर्बा " के प्रथम अंक के लिए आप सभी से प्रविष्टियां आमंत्रित है। कृपया हमें अपनी "अभिरुचि की अभिव्यक्ति" ("expression of interest" ) को नीचे दी गयी लिंक ("गूगल फ़ॉर्म ") पर १५ जुलाई २०२१ या उससे पहले भेजें। "अंतिम प्रविष्टि" rajbhasha@nitgoa.ac.in पर ३१ जुलाई २०२१ को या उससे पहले भेजें। कृपया ध्यान दें कि पत्रिका की रूपरेखा तय करने में हमारी मदद करने के लिए "रुचि की अभिव्यक्ति" प्रस्तुत करना अनिवार्य है। कृपया प्रविष्टि में नीचे दर्शाये गए नियम-निर्देशों का ध्यान रखें। संभावित रूप से यह पत्रिका १४ सितंबर २०२१ को जारी की जाएगी। आप निम्नलिखित रचनायें* भेज सकते है:-
+        h2.poppins-font-resolution.pt-5(style="text-align: justify") हिंदी के उत्सव को हिंदी सप्ताह की निश्चित समय सीमा के अतिरिक्त और विस्तार देते हुए हिंदी पत्रिका "उर्बा " के प्रथम अंक के लिए आप सभी से प्रविष्टियां आमंत्रित है। कृपया हमें अपनी "अभिरुचि की अभिव्यक्ति" ("expression of interest" ) को नीचे दी गयी लिंक ("गूगल फ़ॉर्म ") पर १५ जुलाई २०२१ या उससे पहले भेजें। "अंतिम प्रविष्टि" rajbhasha@nitgoa.ac.in पर ३१ जुलाई २०२१ को या उससे पहले भेजें। कृपया ध्यान दें कि पत्रिका की रूपरेखा तय करने में हमारी मदद करने के लिए "रुचि की अभिव्यक्ति" प्रस्तुत करना अनिवार्य है। कृपया प्रविष्टि में नीचे दर्शाये गए नियम-निर्देशों का ध्यान रखें। संभावित रूप से यह पत्रिका १४ सितंबर २०२१ को जारी की जाएगी। आप निम्नलिखित रचनायें* भेज सकते है:-
         h2.poppins-font-resolution.pt-3(style="text-align: justify") १. लघुकथा (५०० शब्द)
         h2.poppins-font-resolution.pt-3(style="text-align: justify") २. कविता
         h2.poppins-font-resolution.pt-3(style="text-align: justify") ३. ललित निबंध
@@ -19,13 +21,17 @@ div
         h2.poppins-font-resolution.pt-3(style="text-align: justify") ७. पत्रिका के टेम्पलेट का डिजाइन (कवर पेज के साथ)
         h2.poppins-font-resolution.pt-3(style="text-align: justify") पत्रिका के सर्वश्रेष्ठ डिजाइन (कवर पेज के साथ) को १०००/- रुपये का नकद पुरस्कार दिया जाएगा । साथ ही विजेता को पूरी पत्रिका तैयार करने का अवसर दिया जाएगा।
         h2.poppins-font-resolution.pt-3(style="text-align: justify") *राजभाषा समिति के पास रचनाओं के चयन एवं प्रकाशन के सर्वाधिकार सुरक्षित हैं ।
-        h2.poppins-font-resolution.pt-3(style="text-align: justify") अधिक जानकारी के लिए/किसी भी प्रकार के प्रश्न या स्पष्टीकरण के लिए संपर्क करें :
-        h2.poppins-font-resolution.pt-3(style="text-align: justify") अमित वर्मा (18EEE1003, बी.टेक, चतुर्थ वर्ष ): 6377738495; baberwalamit9@gmail.com
-        h2.poppins-font-resolution.pt-3(style="text-align: justify") सौरभ सिंह चौहान (18MCE1022,बी.टेक, चतुर्थ वर्ष ): 7905020037;84.sschauhan@gmail.com
-        h2.poppins-font-resolution.pt-3(style="text-align: justify") "अभिरुचि की अभिव्यक्ति" को प्रस्तुत करने हेतु लिंक ("गूगल फ़ॉर्म "): https://docs.google.com/forms/d/1oWkm2W1-sxmDG2CgbOWz4Rk-cv0k4evD54UufYmxQ7M/edit
-        h2.poppins-font-resolution.pt-3(style="text-align: justify") शुभकामनाएं,
-        h2.poppins-font-resolution.pt-3(style="text-align: justify") शिवनारायण पाटीदार,
-        h2.poppins-font-resolution.pt-3(
+        h2.poppins-font-resolution.pt-8.text-yellow(
+          style="text-align: justify"
+        ) अधिक जानकारी के लिए/किसी भी प्रकार के प्रश्न या स्पष्टीकरण के लिए संपर्क करें :
+        h2.poppins-font-resolution.pt-3(style="text-align: justify") - अमित वर्मा (18EEE1003, बी.टेक, चतुर्थ वर्ष ): 6377738495; baberwalamit9@gmail.com
+        h2.poppins-font-resolution.pt-3(style="text-align: justify") - सौरभ सिंह चौहान (18MCE1022,बी.टेक, चतुर्थ वर्ष ): 7905020037;84.sschauhan@gmail.com
+        h2.poppins-font-resolution.pt-8.text-yellow(
+          style="text-align: center; font-weight: bold"
+        ) "अभिरुचि की अभिव्यक्ति" को प्रस्तुत करने हेतु लिंक ("गूगल फ़ॉर्म "): https://docs.google.com/forms/d/1oWkm2W1-sxmDG2CgbOWz4Rk-cv0k4evD54UufYmxQ7M/edit
+        h2.poppins-font-resolution.pt-8(style="text-align: justify") शुभकामनाएं,
+        h2.poppins-font-resolution.pt-1(style="text-align: justify") शिवनारायण पाटीदार,
+        h2.poppins-font-resolution.pt-1(
           style="text-align: justify; padding-bottom: 2rem"
         ) संयोजक, राजभाषा समिति, राष्ट्रीय प्रौद्योगिकी संस्थान गोवा।
         h2.poppins-font-resolution.pt-3(style="text-align: justify") नियम-निर्देश :
@@ -36,13 +42,18 @@ div
         h2.poppins-font-resolution.pt-3(style="text-align: justify") Greetings from Rajbhasha Samiti!
         h2.poppins-font-resolution.pt-3(style="text-align: justify") All the members of the NIT Goa fraternity including faculty members, staff members, and students are cordially invited to send submissions to our first Hindi Magazine entitled "Urban", including short stories (500 words), poems, essays, humor memoirs/memoirs, jokes, graphic artwork, puzzles, photographs with appropriate captioning, cover page etc. Tentatively, this magazine is set to be released on 14 September 2021. If you would like to contribute, please send us your "expression of interest" to the below given link (Google form) and "final article" to rajbhasha@nitgoa.ac.in on or before 15 July 2021 & 31 July 2021 respectively. Please note that submission of "expression of interest" is mandatory so as to help us in deciding the framework of the magazine.
         h2.poppins-font-resolution.pt-3(style="text-align: justify") Furthermore, design of the template and the cover page of the magazine is also a part of this call. Best design for template of the magazine (with cover page), will be entitled for a cash prize of Rs.1000/-. And the winner for the same will be given an opportunity to prepare the magazine in full.
-        h2.poppins-font-resolution.pt-3(style="text-align: justify") Also go through the instructions given below regarding your entries. All rights reserved for the selection and publication of compositions of the Rajbhasha Samiti. For more information / any type of question or clarification contact:
-        h2.poppins-font-resolution.pt-3(style="text-align: justify") Amit Verma (18EEE1003, B.tech Final year): 6377738495; baberwalamit9@gmail.com
-        h2.poppins-font-resolution.pt-3(style="text-align: justify") Saurabh Singh Chauhan (18MCE1022,B.tech Final year): 7905020037; 84.sschauhan@gmail.com
-        h2.poppins-font-resolution.pt-3(style="text-align: justify") Google link for submission of "expression of interest": https://docs.google.com/forms/d/1oWkm2W1-sxmDG2CgbOWz4Rk-cv0k4evD54UufYmxQ7M/edit
-        h2.poppins-font-resolution.pt-3(style="text-align: justify") Best Wishes,
-        h2.poppins-font-resolution.pt-3(style="text-align: justify") Dr. Shivnarayan Patidar,
-        h2.poppins-font-resolution.pt-3(
+        h2.poppins-font-resolution.pt-3(style="text-align: justify") Also go through the instructions given below regarding your entries. All rights reserved for the selection and publication of compositions of the Rajbhasha Samiti.
+        h2.poppins-font-resolution.pt-3.text-yellow(
+          style="text-align: justify"
+        ) For more information / any type of question or clarification contact:
+        h2.poppins-font-resolution.pt-3(style="text-align: justify") - Amit Verma (18EEE1003, B.tech Final year): 6377738495; baberwalamit9@gmail.com
+        h2.poppins-font-resolution.pt-3(style="text-align: justify") - Saurabh Singh Chauhan (18MCE1022,B.tech Final year): 7905020037; 84.sschauhan@gmail.com
+        h2.poppins-font-resolution.pt-8.text-yellow(
+          style="text-align: center; font-weight: bold"
+        ) Google link for submission of "expression of interest": https://docs.google.com/forms/d/1oWkm2W1-sxmDG2CgbOWz4Rk-cv0k4evD54UufYmxQ7M/edit
+        h2.poppins-font-resolution.pt-8(style="text-align: justify") Best Wishes,
+        h2.poppins-font-resolution.pt-1(style="text-align: justify") Dr. Shivnarayan Patidar,
+        h2.poppins-font-resolution.pt-1(
           style="text-align: justify; padding-bottom: 2rem"
         ) Convenor, Rajbhasha Samiti, NIT Goa
         h2.poppins-font-resolution.pt-3(style="text-align: justify") Instructions:
